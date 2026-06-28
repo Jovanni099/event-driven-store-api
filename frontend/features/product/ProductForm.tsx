6e3e3ef5858535ae56d7ddcb0d6ec79266a143a0
Jovanni099 <jovanni099tmb@gmail.com>
2026-06-28 15:48:22 -0700

    feat(product-form): connect react-hook-form

--- frontend/features/product/ProductForm.tsx
+++ frontend/features/product/ProductForm.tsx
@@ -1,8 +1,30 @@
 'use client';
 
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
 import styles from './ProductForm.module.scss';
 
+import {
+    productSchema,
+    ProductFormData,
+} from './product.schema';
+
 export default function ProductForm() {
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<ProductFormData>({
+        resolver: zodResolver(productSchema),
+
+        defaultValues: {
+            status: 'DRAFT',
+            stock: 0,
+        },
+    });
+
   return (
     <form className={styles.form}>
       <h2 className={styles.title}>Создание товара</h2>
