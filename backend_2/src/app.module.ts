47e67817d3e86ca233898ea43144f4ff48d77d35
Jovanni099 <jovanni099tmb@gmail.com>
2026-09-04 23:47:30 -0700

    feat: configure environment variables

--- backend_2/src/app.module.ts
+++ backend_2/src/app.module.ts
@@ -3,12 +3,17 @@ import { createObserveModule } from '@nestjs/observe';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 
+import { ConfigModule } from '@nestjs/config';
+
 export const { ObserveModule, ObserveInstrument } = createObserveModule();
 
 @Module({
   imports: [
     // Distributed tracing, auto-correlated logs, request/job metrics, error
     // telemetry, alarms, and more — out of the box. Sign up at https://observe.nestjs.com
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     ObserveModule.forRoot({
       appKey: 'YOUR_APP_KEY',
       appSecret: 'YOUR_APP_SECRET',
