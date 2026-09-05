93434c019b48f5ca28218126c945820197bb076d
Jovanni099 <jovanni099tmb@gmail.com>
2026-09-05 01:35:25 -0700

    feat: load port from config

--- backend_2/src/main.ts
+++ backend_2/src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule, ObserveInstrument } from './app.module.js';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     instrument: ObserveInstrument,
   });
-  await app.listen(process.env.PORT ?? 3333);
+
+  const configService = app.get(ConfigService);
+
+  const port = configService.get<number>('PORT') ?? 3333;
+
+  await app.listen(port);
 }
 await bootstrap();
