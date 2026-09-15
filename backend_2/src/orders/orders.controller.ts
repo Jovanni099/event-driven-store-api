586816240dcbce61955e2558c5b952a7d698f429
Jovanni099 <jovanni099tmb@gmail.com>
2026-09-15 10:51:57 -0700

    feat: connect orders controller to service

--- backend_2/src/orders/orders.controller.ts
+++ backend_2/src/orders/orders.controller.ts
@@ -1,15 +1,18 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto.js';
+import { OrdersService } from './orders.service.js';
 
 @Controller('orders')
 export class OrdersController {
+  constructor(private readonly orderService: OrdersService) {}
+
   @Post()
   createOrder(@Body() dto: CreateOrderDto) {
-    console.log('Received order:', dto);
-
-    return {
-      message: 'Order received',
-      order: dto,
-    };
+    // console.log('Received order:', dto);
+    // return {
+    //   message: 'Order received',
+    //   order: dto,
+    // };
+    return this.orderService.createOrder(dto);
   }
 }
